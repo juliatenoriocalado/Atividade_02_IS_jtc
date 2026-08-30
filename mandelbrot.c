13df35c08f91ccab7a0dc74d26e7baa67bed7713
juliatenoriocalado <jtc@cesar.school>
2026-08-30 15:49:05 -0300

    Implementando  a primeira versao com Pthreads dividindo a imagem em blocos de linhas

--- mandelbrot.c
+++ mandelbrot.c
@@ -5,6 +5,7 @@
 #include <time.h>
 #include <stdint.h>
 #include <omp.h>
+#include <pthread.h>
 
 typedef struct mandelbrot{
     int largura;
@@ -12,6 +13,50 @@ typedef struct mandelbrot{
     int max_interacoes;
     int numeroDeThreads;
 } Mandelbrot;
+
+typedef struct DadosThread {
+    int *imagem;
+    Mandelbrot *dados;
+    int inicio_linha;
+    int fim_linha;
+} DadosThread;
+
+void *funcao_thread(void *arg){
+
+    DadosThread *dt = (DadosThread *) arg;
+    
+    for (int linha = dt->inicio_linha; linha < dt->fim_linha; linha++){
+        for (int coluna=0; coluna<dt->dados->largura; coluna++){
+            
+            int indice = linha * dt->dados->largura + coluna;
+
+            double c_real = -2.0 + ((double) coluna/ (dt->dados->largura - 1)) * 3.0;
+            double c_imagem = -1.5 + ((double) linha / (dt->dados->altura - 1)) * 3.0;
+            double z_real = 0.0;
+            double z_imagem = 0.0;
+            int iteracoes = 0;
+            
+            while (iteracoes < dt->dados->max_interacoes){
+                double novo_real = (z_real * z_real) - (z_imagem * z_imagem) + c_real;
+                double novo_imagem = (2 * z_real * z_imagem) + c_imagem;
+
+                z_real = novo_real;
+                z_imagem = novo_imagem;
+
+                iteracoes++;
+
+                if ((z_real * z_real) + (z_imagem * z_imagem) > 4.0){ //se explodiu
+                    break;
+                }
+            }
+
+            int intensidade = (int)(((double) iteracoes / dt->dados->max_interacoes) * 255.0);
+            dt->imagem[indice] = intensidade;
+
+        }    
+    }
+    return NULL;
+}
 
 int main(int argc, char *argv[]){
 
@@ -282,21 +327,101 @@ int main(int argc, char *argv[]){
     }
 
     fclose(arquivo);
- 
- 
+
+    pthread_t *threads = malloc(dados.numeroDeThreads * sizeof(pthread_t));
+    DadosThread *args = malloc(dados.numeroDeThreads * sizeof(DadosThread));
+
+    if (threads == NULL || args == NULL){
+        fprintf(stderr, "Erro ao alocar memoria para as threads.\n");
+        free(threads);
+        free(args);
+        free(imagem);
+        exit(1);
+    }
+
+    struct timespec inicio_thread;
+    struct timespec fim_thread;
+
+    clock_gettime(CLOCK_MONOTONIC, &inicio_thread);
+
+    int threads_criadas = 0;
+
+    for (int i=0; i<dados.numeroDeThreads; i++){
+
+        args[i].imagem = imagem;
+        args[i].dados = &dados;
+
+        args[i].inicio_linha = i * dados.altura / dados.numeroDeThreads;
+
+        args[i].fim_linha = (i + 1) * dados.altura / dados.numeroDeThreads;
+
+        int resultado = pthread_create(&threads[i], NULL, funcao_thread, &args[i]);
+
+        if (resultado != 0){
+            fprintf(stderr, "Erro ao criar thread.\n");
+            break;
+        }
+
+        threads_criadas++;
+            
+    }
+
+    for (int i = 0; i < threads_criadas; i++){
+        pthread_join(threads[i], NULL);
+    }
+
+    if (threads_criadas != dados.numeroDeThreads)
+    {free(imagem);
+    free(threads);
+    free(args);
+    exit(1);
+    }
+
+    clock_gettime(CLOCK_MONOTONIC, &fim_thread);
+
+    double tempo_pthreads1 =
+        (fim_thread.tv_sec - inicio_thread.tv_sec) +
+        (fim_thread.tv_nsec - inicio_thread.tv_nsec) / 1000000000.0;
+
+    arquivo = fopen("mandelbrot_jtc_pthreads1.pgm", "w");
+
+    if (arquivo == NULL){
+        fprintf(stderr, "Erro ao abrir ou criar arquivo Pthreads1.\n");
+        free(imagem);
+        free(threads);
+        free(args);
+        exit(1);
+    }
+
+    for (int linha = 0; linha < dados.altura; linha++){
+        for (int coluna = 0; coluna < dados.largura; coluna++){
+            int indice = linha * dados.largura + coluna;
+            fprintf(arquivo, "%d ", imagem[indice]);
+        }
+
+        fprintf(arquivo, "\n");
+    }
+
+    fclose(arquivo);
+    
     FILE *arquivo_tempos;
     arquivo_tempos = fopen("times.txt", "w");
 
     if (arquivo_tempos == NULL){
         fprintf(stderr, "Erro ao abrir ou criar times.txt.\n");
         free(imagem);
+        free(threads);
+        free(args);
         exit(1);
     }
 
     fprintf(arquivo_tempos, "Serial: %f\n", tempo_serial);
     fprintf(arquivo_tempos, "OpenMP: %f\n", tempo_openmp);
+    fprintf(arquivo_tempos, "Pthreads1: %f\n", tempo_pthreads1);
     fclose(arquivo_tempos);
     free(imagem);
+    free(threads);
+    free(args);
 
     return 0;
 }
